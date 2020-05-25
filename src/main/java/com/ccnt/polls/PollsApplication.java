aad8e0f64bcf1816c3acffb43718ae90277bc416
liuhu <Huawei.Liu@pointclickcare.com>
2020-05-25 19:48:55 -0400

    Init Commit

---
+++ src/main/java/com/ccnt/polls/PollsApplication.java
@@ -0,0 +1,13 @@
+package com.ccnt.polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
+
+}
