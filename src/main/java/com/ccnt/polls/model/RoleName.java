f272300d485849c242f4a8442a32304737515467
liuhu <Huawei.Liu@pointclickcare.com>
2020-05-25 22:07:58 -0400

    Part

---
+++ src/main/java/com/ccnt/polls/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.ccnt.polls.model;
+
+public enum  RoleName {
+  ROLE_USER,
+  ROLE_ADMIN
+}
---
+++ src/main/java/com/ccnt/polls/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.ccnt.polls.repository;
+
+import com.ccnt.polls.model.Role;
+import com.ccnt.polls.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
